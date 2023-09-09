3ba226b4af483bcbbd4586dd55cd0245f536e31e
Brian-w-m <111062353+Brian-w-m@users.noreply.github.com>
2023-09-10 00:27:47 +1000

    Update 2004 A1.py

--- 2004 A1.py
+++ 2004 A1.py
@@ -1,9 +1,13 @@
-def restaurantFinder(d, site_list): #[50, 10, 12, 65, 40, 95, 100, 12, 20, 30]
+def restaurantFinder(d, site_list): #[50*, 10, 12, 65*, 40, 95*, 100, 12*, 20] + [30*] [1,4,6,8,10]
     chosen_sites = []
-    return restaurantFinder_aux(d, site_list, chosen_sites)
+    total_earnings = 0
+    restaurantFinder_aux(d, site_list, chosen_sites, total_earnings)
+    return chosen_sites
 
-def restaurantFinder_aux(d, site_list, chosen_sites):
+def restaurantFinder_aux(d, site_list, chosen_sites, total_earnings):
+    if site_list[-1] > site_list[-2]:
+        if
     if site_list == []:
         return  0
     else:
-        return restaurantFinder_aux(d, site_list, chosen_sites)
+        return restaurantFinder_aux(d, site_list.pop(), chosen_sites)
