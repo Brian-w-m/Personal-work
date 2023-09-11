a6d4ac5a9187527ea725ae281c26375619ac0859
Brian-w-m <bwuu0030@student.monash.edu>
2023-09-12 05:00:49 +1000

    Added test cases and changes to A1

--- FIT2004A1.py
+++ FIT2004A1.py
@@ -1,9 +1,8 @@
 def restaurantFinder(d, site_list):
     max_profits = [0] * len(site_list)
-    max_profits[0] = site_list[0]
     
     # Need first loop since we cannot assume d < len(site_list)
-    for i in range(1,min(d+1, len(site_list))):
+    for i in range(0,min(d+1, len(site_list))):
         if site_list[i] > max_profits[i-1]:
             max_profits[i] = site_list[i]
         else:
@@ -19,7 +18,7 @@ def restaurantFinder(d, site_list):
     rests = []  
     k = len(site_list)-1
     while k>=0:
-        if k == 0:
+        if k == 0 and max_profits[0]>0:
             rests.append(1)
         if max_profits[k]> max_profits[k-1]:
             rests.append(k+1)
@@ -27,7 +26,7 @@ def restaurantFinder(d, site_list):
         else:
             k = k-1
 
-    return max_profits, rests[::-1]
+    return max_profits[-1], rests[::-1]
 
 
 
@@ -37,3 +36,7 @@ print(restaurantFinder(3, [50, 10, 12, 65, 40, 95, 100, 12, 20, 30])) #175 [1,6,
 print(restaurantFinder(7, [50, 10, 12, 65, 40, 95, 100, 12, 20, 30])) #100 [7]
 print(restaurantFinder(0, [50, 10, 12, 65, 40, 95, 100, 12, 20, 30])) #434 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(restaurantFinder(7, [1]))
+
+class graph:
+    def __init__(self, paths, keys):
+        self.paths = paths
